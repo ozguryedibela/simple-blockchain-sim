655d44844283bdd3b5a4d6f74c3b879ea2a140af
ozguryedibela <oyedibela21@gmail.com>
2025-11-22 23:24:10 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 #include <ctime>
 #include <vector>
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-// --- to be able to use sha256 ---
+// --- to be able to use sha256 algorithm ---
 string sha256(string text) {
     SHA256_CTX ctx;
     BYTE hash[SHA256_BLOCK_SIZE];
@@ -51,10 +51,14 @@ public:
         return sha256(ss.str());
     }
 
+    string getHash() {
+        return blockHash;
+    }
+
     void transactionLog(string data) {
         ofstream file("transactions.log", ios::app);
         if (file.is_open()) {
-            file << data << endl;
+            file << data << ", Hash: " << getHash() << endl << endl;
             file.close();
         }
         else {
@@ -64,18 +68,205 @@ public:
 
 };
 
+//class User {
+//private:
+//    float balance,coins;
+//    string name;
+//public:
+//    User(float blnc, float coinx, string namex) {
+//        balance = blnc;
+//        coins = coinx;
+//        name = namex;
+//    }
+//};
+
+string depositWithdrawMoney(float *initialBalance) {
+    float deposit;
+    char button;
+    cout << "\n1 - Deposit Money\n2 - Withdraw Money\n3 - Menu\n\n";
+    cin >> button;
+
+    switch (button)
+    {
+    case '1': {
+        cout << "\nEnter the amount you want to deposit: ";
+        cin >> deposit;
+        *initialBalance += deposit;
+        stringstream message;
+        message << "Deposited -> MyAccount " << deposit << " dollars.";
+        return message.str();
+    }
+
+    case '2': {
+        cout << "\nEnter the amount you want to withdraw: ";
+        cin >> deposit;
+        if (deposit > *initialBalance) {
+            cout << "\nNot enough balance!\n";
+            break;
+        }
+        *initialBalance -= deposit;
+        stringstream message;
+        message << "MyAccount -> Withdrawed " << deposit << " dollars.";
+        return message.str();
+    }
+
+    case '3':
+        return"";
+
+    default:
+        return"";
+    }
+}
+
+string buySellCoin(float *initialBalance, float *initialCoins) {
+    float coinPrice = 3.5,amount = 0;
+    char button;
+
+    cout << "\nCoin Price: " << coinPrice << "\nBalance: " << *initialBalance << "\nCoins: " << *initialCoins << "\n\n1 - Buy\n2 - Sell\n3 - Menu\n\n";
+    cin >> button;
+
+    switch (button)
+    {
+    case '1': {
+        cout << "\nEnter the coin amount you want to buy: ";
+        cin >> amount;
+        if (amount * coinPrice > *initialBalance) {
+            cout << "\nNot enough balance!\n";
+            break;
+        }
+        *initialBalance = *initialBalance - (amount * coinPrice);
+        *initialCoins = *initialCoins + amount;
+        stringstream message;
+        message << "User -> MyAccount " << amount << " coins.";
+        return message.str();
+    }
+
+    case '2': {
+        cout << "\nEnter the coin amount you want to sell: ";
+        cin >> amount;
+        if (amount > *initialCoins) {
+            cout << "\nNot enough coins!\n";
+            break;
+        }
+        *initialBalance = *initialBalance + (amount * coinPrice);
+        *initialCoins = *initialCoins - amount;
+        stringstream message;
+        message << "MyAccount -> User " << amount << " coins.";
+        return message.str();
+    }
+
+    case'3':
+        return"";
+
+    default:
+        return"";
+    }
+}
+
+void saveChanges(float initialBalance, float initialCoins) {
+
+    ofstream fileClear("asset_information.txt", ios::trunc);
+    if (fileClear.is_open()) {
+        fileClear.clear();
+        fileClear.close();
+    }
+    else {
+        cout << "\nFailed to clear old informations!\n";
+    }
+
+    ofstream file("asset_information.txt");
+    if (file.is_open()) {
+        file << initialBalance << endl << initialCoins;
+        file.close();
+    }
+    else {
+        cout << "\nFailed to save changes!\n";
+    }
+}
+
+
+
 int main() {
 
-    Block b0(0, "Genesis Block", "0");
+    ofstream clearTransactions("transactions.log", ios::trunc);
+    clearTransactions.clear();
+
+    string fileName = "asset_information.txt";
+    ifstream control(fileName);
+
+    if (!control.is_open()) {
+        ofstream creator(fileName);
+        creator << "0" << endl << "0";
+        creator.close();
+        cout << "New save file created.\n\n\n" << endl;
+    }
+    else {
+        control.close();
+    }
+
+    string informations[5];  
+    int i = 0;
+
+    ifstream file("asset_information.txt");
+
+    if (file.is_open()) {
+        while (getline(file, informations[i])) {
+            i++;
+        }
+        file.close();
+    }
+    else {
+        cout << "\nFailed to read account informations!\n";
+    }
+
+    float balance = stof(informations[0]), coins = stof(informations[1]);
+    char button;
+    int blockIndex = 0;
+
+    //starter block
+    Block b0(blockIndex, "Genesis Block", "0");
     cout << "Block 0 Hash: " << b0.calculateHash() << endl;
-    
-    Block b1(1, "Ahmet -> Mehmet 5 BTC", b0.calculateHash());
-    cout << "Block 1 Hash: " << b1.calculateHash() << endl;
 
-    Block b2(2, "Mehmet -> Ahmet 2 BTC", b1.calculateHash());
-    cout << "Block 2 Hash: " << b2.calculateHash() << endl;
+    vector<Block>blockVector = {b0};
 
-    Block b3(3, "Ahmet -> Mehmet 15 BTC", b2.calculateHash());
-    cout << "Block 3 Hash: " << b3.calculateHash() << endl;
+    do {
+        cout << "\n__Control Panel__\n\n" << "1 - Deposit/Withdraw Money\n2 - Buy/Sell Coin\n3 - Quit\n\nBalance: " << balance << endl <<"Coins: " << coins << "\n\n";
+        cin >> button;
 
+        switch (button)
+        {
+        case '1': {
+            string result = depositWithdrawMoney(&balance);
+
+            //to prevent creating a hash if no operation made
+            if (result == "") {
+                continue;
+            }
+
+            blockIndex++;
+            Block currentBlock(blockIndex, result, blockVector.back().getHash());
+            blockVector.push_back(currentBlock);
+            continue;
+        }
+        case '2': {
+            string result = buySellCoin(&balance, &coins);
+            if (result == "") {
+                continue;
+            }
+
+            blockIndex++;
+            Block currentBlock(blockIndex, result, blockVector.back().getHash());
+            blockVector.push_back(currentBlock);
+            continue;
+        }
+        case '3': {
+            saveChanges(balance, coins);
+            cout << "\nChanges Saved. Goodbye.";
+            continue;
+        }
+        default:
+            continue;
+        }
+
+    } while (button != '3');
 }
